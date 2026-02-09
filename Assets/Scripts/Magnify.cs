ea27746a83df67678f9bbe6adbadc54f13984ac9
Helena Paakki <hpaakki23@student.oulu.fi>
2026-02-09 14:55:28 +0200

    Homework 2 done

--- Assets/Scripts/Magnify.cs
+++ Assets/Scripts/Magnify.cs
@@ -4,6 +4,7 @@ public class Magnify : MonoBehaviour
 {
     public Transform magnefying;
     public Transform lens;
+    public Camera mag_camera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,5 +19,7 @@ public class Magnify : MonoBehaviour
         Vector3 newRotation = lens.eulerAngles;
         newRotation.z = 0;
         lens.eulerAngles = newRotation;
+
+        mag_camera.nearClipPlane = Vector3.Distance(magnefying.position, lens.position);
     }
 }
