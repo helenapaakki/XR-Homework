5761beccf7bc325ee117df61a4562439326a8ada
Helena Paakki <paakkih@gmail.com>
2026-02-24 11:46:02 +0200

    collider code and faraway grabbing code (untested)

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Hand : MonoBehaviour
 {
-    public float speed;
+    public float animationSpeed;
     Animator animator;
     private float gripTarget;
     private float triggerTarget;
@@ -12,6 +12,15 @@ public class Hand : MonoBehaviour
     private float triggerCurrent;
     private string animatorGripParam = "Grip";
     private string animatorTriggerParam = "Trigger";
+
+    public GameObject followObject;
+    public float followSpeed = 30f;
+    public float rotateSpeed = 100f;
+    public Vector3 positionOffSet;
+    public Vector3 rotationOffSet;
+    private Transform followTarget;
+    private Rigidbody body;
+
     internal void SetGrip(float v)
     {
         gripTarget = v;
@@ -26,26 +35,49 @@ public class Hand : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
+
+        followTarget = followObject.transform;
+        body = GetComponent<Rigidbody>();
+        body.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        body.interpolation = RigidbodyInterpolation.Interpolate;
+        body.mass = 20f;
+
+        body.position = followTarget.position;
+        body.rotation = followTarget.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
         AnimateHand();
+
+        PhysicsMove();
     }
 
     void AnimateHand()
     {
         if (gripCurrent != gripTarget)
         {
-            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * animationSpeed);
             animator.SetFloat(animatorGripParam, gripCurrent);
             Debug.Log(gripCurrent);
         }
         if (triggerCurrent != triggerTarget)
         {
-            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * animationSpeed);
             animator.SetFloat(animatorTriggerParam, triggerCurrent);
         }
+    }
+
+    void PhysicsMove()
+    {
+        var positionWithOffSet = followTarget.position + positionOffSet;
+        var distance = Vector3.Distance(positionWithOffSet, transform.position);
+        body.linearVelocity = (positionWithOffSet- transform.position).normalized * (followSpeed * distance);
+
+        var rotationWithOffSet = followTarget.rotation * Quaternion.Euler(rotationOffSet);
+        var q = rotationWithOffSet * Quaternion.Inverse(body.rotation);
+        q.ToAngleAxis(out float angle, out Vector3 axis);
+        body.angularVelocity = axis * (angle * Mathf.Deg2Rad * rotateSpeed);
     }
 }
